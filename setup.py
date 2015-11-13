6ccb4114fa573cf51ba0b3d3a84402e719731580
tuxpy <q8886888@qq.com>
2015-11-13 16:22:22 +0800

    添加查看数据表字段方法columns()

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author        : tuxpy
+# Email         : q8886888@qq.com.com
+# Last modified : 2015-11-05 10:50:34
+# Filename      : setup.py
+# Description   : 
+from distutils.core import setup
+
+setup(
+        name = 'mpsql',
+        version = '0.6',
+        author = 'moment-x',
+        py_modules = ['mpsql'],
+        )
+
